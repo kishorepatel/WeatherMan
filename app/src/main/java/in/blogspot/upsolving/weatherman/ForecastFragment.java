79d1045ee2b0fbd806db558008ae3352d92c8c64
kishorepatel <kishore.patel.india@gmail.com>
2015-09-24 13:10:35 +0530

    L2 - created subclass ForecastFragment.FetchWeatherTast extends AsyncTask

--- app/src/main/java/in/blogspot/upsolving/weatherman/ForecastFragment.java
+++ app/src/main/java/in/blogspot/upsolving/weatherman/ForecastFragment.java
@@ -1,5 +1,6 @@
 package in.blogspot.upsolving.weatherman;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -50,67 +51,65 @@ public class ForecastFragment extends Fragment {
 		listItems.setAdapter(mForeCastAdapter);
 
 
-		//-----------------HttpURLConnection----------------
-		HttpURLConnection urlConnection = null;
-		BufferedReader reader = null;
-
-		// Will contain the raw JSON response as a string.
-		String forecastJsonStr = null;
-
-		try {
-			// Construct the URL for the OpenWeatherMap query
-			// Possible parameters are avaiable at OWM's forecast API page, at
-			// http://openweathermap.org/API#forecast
-			URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=chennai,in&mode=json&cnt=7");
-
-			// Create the request to OpenWeatherMap, and open the connection
-			urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("GET");
-			urlConnection.connect();
-
-			// Read the input stream into a String
-			InputStream inputStream = urlConnection.getInputStream();
-			StringBuffer buffer = new StringBuffer();
-			if (inputStream == null) {
-				// Nothing to do.
-				System.err.println("input stream is null: return null");
-				return null;
-			}
-			reader = new BufferedReader(new InputStreamReader(inputStream));
-
-			String line;
-			while ((line = reader.readLine()) != null) {
-				// Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-				// But it does make debugging a *lot* easier if you print out the completed
-				// buffer for debugging.
-				buffer.append(line + "\n");
-			}
-
-			if (buffer.length() == 0) {
-				// Stream was empty.  No point in parsing.
-				System.err.println("buffer is null: return null");
-				return null;
-			}
-			forecastJsonStr = buffer.toString();
-		} catch (IOException e) {
-			Log.e("PlaceholderFragment", "Error ", e);
-			// If the code didn't successfully get the weather data, there's no point in attemping
-			// to parse it.
-			return null;
-		} finally{
-			if (urlConnection != null) {
-				urlConnection.disconnect();
-			}
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (final IOException e) {
-					Log.e("PlaceholderFragment", "Error closing stream", e);
-				}
-			}
-		}
-
-		//-----------------------------------------------------------------------------------------------------------------------
 		return v;
 	}//onCreateView
+
+
+
+
+	private class FetchWeatherTask extends AsyncTask<String, Void, Void>{
+		@Override
+		protected Void doInBackground(String... params) {
+			HttpURLConnection urlConnection = null;
+			BufferedReader reader = null;
+			String forecastJsonString = null;
+
+			try{
+				//NOTE: when using execute() method => please pass url as first param
+				URL url = new URL(params[0]);
+				urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod("GET");
+				urlConnection.connect(); //this line creates lot of trouble
+
+				InputStream inputStream = urlConnection.getInputStream();
+				if(inputStream == null){
+					return null;
+				}
+
+				reader = new BufferedReader(new InputStreamReader(inputStream));
+				StringBuffer buffer = new StringBuffer();
+				String line = "";
+
+				while((line = reader.readLine()) != null){
+					buffer.append(line + "\n");
+				}
+
+				if(buffer.length() == 0){
+					return null;
+				}
+
+				forecastJsonString = buffer.toString();
+				Log.v("JSON: " , forecastJsonString);
+			}
+			catch(IOException e){
+				Log.e("In FetchWeatherTask: " , "Error: ", e);
+			}
+			finally{
+				if(urlConnection != null){
+					urlConnection.disconnect();
+				}
+				if(reader != null){
+					try{
+						reader.close();
+					}
+					catch(IOException e){
+						Log.e("In FetchWeatherTask: ", "Error closing stream: " , e);
+					}
+				}
+			}
+
+			return null;
+		}//doInBackGround
+
+	}//FetchWeatherTask
 }
